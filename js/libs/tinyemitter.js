68a468f38eff365a2d893d743c067903e66c8e21
myw <myw@mywdeMacBook-Air.local>
2026-03-21 18:46:14 +0800

    重写tinyemitter.js，使其兼容微信小游戏环境

--- js/libs/tinyemitter.js
+++ js/libs/tinyemitter.js
@@ -1,1 +1,75 @@
-(function(e){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=e()}else if(typeof define==="function"&&define.amd){define([],e)}else{var n;if(typeof window!=="undefined"){n=window}else if(typeof global!=="undefined"){n=global}else if(typeof self!=="undefined"){n=self}else{n=this}n.TinyEmitter=e()}})(function(){var e,n,t;return function r(e,n,t){function i(o,u){if(!n[o]){if(!e[o]){var s=typeof require=="function"&&require;if(!u&&s)return s(o,!0);if(f)return f(o,!0);var a=new Error("Cannot find module '"+o+"'");throw a.code="MODULE_NOT_FOUND",a}var l=n[o]={exports:{}};e[o][0].call(l.exports,function(n){var t=e[o][1][n];return i(t?t:n)},l,l.exports,r,e,n,t)}return n[o].exports}var f=typeof require=="function"&&require;for(var o=0;o<t.length;o++)i(t[o]);return i}({1:[function(e,n,t){function r(){}r.prototype={on:function(e,n,t){var r=this.e||(this.e={});(r[e]||(r[e]=[])).push({fn:n,ctx:t});return this},once:function(e,n,t){var r=this;function i(){r.off(e,i);n.apply(t,arguments)}i._=n;return this.on(e,i,t)},emit:function(e){var n=[].slice.call(arguments,1);var t=((this.e||(this.e={}))[e]||[]).slice();var r=0;var i=t.length;for(r;r<i;r++){t[r].fn.apply(t[r].ctx,n)}return this},off:function(e,n){var t=this.e||(this.e={});var r=t[e];var i=[];if(r&&n){for(var f=0,o=r.length;f<o;f++){if(r[f].fn!==n&&r[f].fn._!==n)i.push(r[f])}}i.length?t[e]=i:delete t[e];return this}};n.exports=r;n.exports.TinyEmitter=r},{}]},{},[1])(1)});
+// TinyEmitter库 - 事件发射器
+// 兼容微信小游戏、浏览器和Node.js环境
+
+(function(global, factory) {
+  // 微信小游戏环境
+  if (typeof wx !== 'undefined' && typeof wx.createCanvas !== 'undefined') {
+    global.TinyEmitter = factory();
+  }
+  // 浏览器环境
+  else if (typeof window !== 'undefined') {
+    window.TinyEmitter = factory();
+  }
+  // Node.js环境
+  else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = factory();
+  }
+  // 其他环境
+  else {
+    global.TinyEmitter = factory();
+  }
+}(typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : this, function() {
+  function Emitter() {
+    this.e = {};
+  }
+
+  Emitter.prototype.on = function(name, fn, ctx) {
+    if (!this.e[name]) {
+      this.e[name] = [];
+    }
+    this.e[name].push({ fn: fn, ctx: ctx });
+    return this;
+  };
+
+  Emitter.prototype.once = function(name, fn, ctx) {
+    var self = this;
+    function on() {
+      self.off(name, on);
+      fn.apply(ctx, arguments);
+    }
+    on._ = fn;
+    return this.on(name, on, ctx);
+  };
+
+  Emitter.prototype.emit = function(name) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    var events = this.e[name] || [];
+    for (var i = 0; i < events.length; i++) {
+      events[i].fn.apply(events[i].ctx, args);
+    }
+    return this;
+  };
+
+  Emitter.prototype.off = function(name, fn) {
+    var events = this.e[name] || [];
+    var remaining = [];
+    for (var i = 0; i < events.length; i++) {
+      if (events[i].fn !== fn && events[i].fn._ !== fn) {
+        remaining.push(events[i]);
+      }
+    }
+    if (remaining.length) {
+      this.e[name] = remaining;
+    } else {
+      delete this.e[name];
+    }
+    return this;
+  };
+
+  // 返回构造函数
+  function TinyEmitter() {}
+  TinyEmitter.prototype = Emitter.prototype;
+  TinyEmitter.prototype.constructor = TinyEmitter;
+
+  return TinyEmitter;
+}));
